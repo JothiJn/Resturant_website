cfa23274ef7bc578b7a3c6aec3693ef4efdc9230
Jothinathan <jothinathan12334@gmail.com>
2026-04-13 18:57:47 +0530

    Delete src directory

--- src/app/menu/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MenuClient from "./MenuClient";
-import { getMenuItems } from "../actions/admin";
-
-export default async function MenuPage() {
-  const { data: initialDishes } = await getMenuItems();
-
-  return <MenuClient initialDishes={initialDishes} />;
-}
